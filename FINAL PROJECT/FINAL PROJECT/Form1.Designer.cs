f1615672ea4c81f90b24971febc39cfa062f9230
Clayton Aylor <143904011+clayton8090@users.noreply.github.com>
2023-09-21 11:27:58 -0500

    Add files via upload

---
+++ FINAL PROJECT/FINAL PROJECT/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace FINAL_PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(58, 102);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1322, 224);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(466, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Display Patient Status ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.FlatAppearance.BorderSize = 5;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(637, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear Monitor";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.FlatAppearance.BorderSize = 5;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(802, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 67);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Patient";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(267, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Temp 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(410, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Temp 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(557, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Temp 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(696, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Temp 4 ";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(827, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 40);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Lowest Dropped";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(979, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 40);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Highest Dropped";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label8.Location = new System.Drawing.Point(1122, 60);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 40);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Average Temp";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(1277, 60);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 40);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Status ";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(54, 79);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 20);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1445, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ FINAL PROJECT/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FINAL_PROJECT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        string patient;
+        double temp1, temp2, temp3, temp4, lowDrop, highDrop, avgTemp;
+        int rowID;
+        string status;
+
+        StreamReader finalData;
+
+        private void button1_Click(object sender, EventArgs e)
+        {//Display Patient Status Button
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+
+            {
+                finalData = File.OpenText(openFileDialog1.FileName);
+                rowID = 1;
+
+                while (!finalData.EndOfStream)
+                {
+                    GetData();
+                    double lowDrop = DropLowTemp();
+                    double highDrop = DropHighTemp();
+                    double avgTemp = GetAVGTemp();
+                    GetStatus();
+                    OutPutData();
+
+                    rowID ++;
+                }
+            }
+
+
+
+        }
+        private void GetData()
+        {
+            try
+            {
+                patient = finalData.ReadLine();
+                temp1 = double.Parse(finalData.ReadLine());
+                temp2 = double.Parse(finalData.ReadLine());
+                temp3 = double.Parse(finalData.ReadLine());
+                temp4 = double.Parse(finalData.ReadLine());
+            }
+
+            catch (FormatException ex) 
+            {
+                MessageBox.Show("Invalid input format. Please enter valid temperature values.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+        }
+
+        double DropLowTemp()
+        {
+            try
+            {
+                double[] temperatures = { temp1, temp2, temp3, temp4 };
+                Array.Sort(temperatures);
+
+                lowDrop = temperatures[0];
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while trying to drop the lowest temperature: {ex.Message}");
+                lowDrop = 0;
+            }
+
+            return lowDrop;
+
+        }
+
+        double DropHighTemp()
+         {
+
+            try
+            {
+                double[] temperatures = { temp1, temp2, temp3, temp4 };
+                Array.Sort(temperatures);
+
+                highDrop = temperatures[3];
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show($"An error occurred while trying to drop the highest temperature: {ex.Message}");
+                highDrop = 0; 
+            }
+           
+            return highDrop;
+
+         }
+
+        
+         double GetAVGTemp()
+         {
+            try
+            {
+                double[] temperatures = { temp1, temp2, temp3, temp4 };
+                Array.Sort(temperatures);
+
+                avgTemp = (temperatures[1] + temperatures[2]) / 2.0;
+            }
+            
+            catch(Exception ex)
+            {
+                MessageBox.Show($"An error occurred while trying to calculate the average temperature: {ex.Message}");
+                avgTemp = 0;
+            }
+
+            return avgTemp;
+         }
+        
+
+
+
+        private void GetStatus()
+        {
+            try
+            {
+                if (avgTemp > 99)
+                {
+                    status = "Fever";
+                }
+                else if (avgTemp < 97)
+                {
+                    status = "LowTemp";
+                }
+                else
+                {
+                    status = "Normal";
+                }
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show($"An error occurred while trying get the patient status: {ex.Message}");
+                status = "";
+            }
+
+        }
+
+        private void OutPutData()
+        {
+            try
+            {
+                listBox1.Items.Add(rowID.ToString() + "\t" +
+                        patient.PadRight(15) + "\t" +
+                        temp1.ToString("F2").PadRight(20) + "\t" +
+                        temp2.ToString("F2").PadRight(20) + "\t" +
+                        temp3.ToString("F2").PadRight(20) + "\t" +
+                        temp4.ToString("F2").PadRight(20) + "\t" +
+                        lowDrop.ToString("F2").PadRight(20) + "\t" +
+                        highDrop.ToString("F2").PadRight(20) + "\t" +
+                        avgTemp.ToString("F2").PadRight(20) + "\t" +
+                        status.ToString().PadRight(20));
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error when displaying data to listbox! " + ex.Message);
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//Clear Display Button
+           
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {//Exit Button
+            this.Close();
+            finalData.Close();
+        }
+
+    }
+}
